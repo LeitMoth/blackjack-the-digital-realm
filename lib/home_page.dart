3a81cfb661677605aa749ab3ba143091c5f7576a
hamjac5334 <50788084+hamjac5334@users.noreply.github.com>
2024-10-10 00:59:55 -0500

    Updated Authentication Screens (pulled from Project 5)

---
+++ lib/home_page.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart' // new
+    hide EmailAuthProvider, PhoneAuthProvider;    // new
+import 'package:flutter/material.dart';           // new
+import 'package:provider/provider.dart';          // new
+
+import 'app_state.dart';                          // new
+import 'src/authentication.dart';                 // new
+import 'src/widgets.dart';                  // new
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Blackjack: the digital realm'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          // Add from here
+          Consumer<ApplicationState>(
+            builder: (context, appState, _) => AuthFunc(
+                loggedIn: appState.loggedIn,
+                signOut: () {
+                  FirebaseAuth.instance.signOut();
+                }),
+          ),
+          // to here
+          const Divider(
+            height: 8,
+            thickness: 1,
+            indent: 8,
+            endIndent: 8,
+            color: Colors.grey,
+          ),
+        ],
+      ),
+    );
+  }
+}
